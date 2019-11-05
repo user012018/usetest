948a6fc739c7a127e13df89fb42dc7142f48d49f
user012018 <57412720+user012018@users.noreply.github.com>
2019-11-05 22:51:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // (function(System, SystemJS) {(function (require, exports, module, __filename, __dirname, global, GLOBAL, process) {"use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
 var test_1 = require("./js/test");
+
 // Write Javascript code!
 // var appDiv = document.getElementById('app');
 // appDiv.innerHTML = test_1.test();
