0641c73243b5a2f207d17a68b1ce3d556dbcd7af
mrbannyjo <mrbannyjo@gmail.com>
2019-11-05 21:18:02 +0300

    test after reinstall
    

---
+++ index.js
@@ -0,0 +1,10 @@
+// (function(System, SystemJS) {(function (require, exports, module, __filename, __dirname, global, GLOBAL, process) {"use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+var test_1 = require("./js/test");
+// Write Javascript code!
+// var appDiv = document.getElementById('app');
+// appDiv.innerHTML = test_1.test();
+
+// }).apply(__cjsWrapper.exports, __cjsWrapper.args);
+// })(System, System);
+//# sourceURL=https://js-ozuhtv.stackblitz.io/~/index.js
